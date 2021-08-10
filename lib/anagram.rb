1c2562898bf12683219512adad684fe91f93a0f0
erobbi <elirobbins0@gmail.com>
2021-08-10 16:55:28 -0400

    completed

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,14 @@
 # Your code goes here!
+require 'pry'
+
+class Anagram
+    attr_reader :word
+    def initialize(word)
+        @name = word
+    end
+
+    def match(array)
+        array.select{|word| word.split("").sort == @name.split("").sort}
+    end
+
+end
